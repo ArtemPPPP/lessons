6592148ebf4ead4419e06def043df11c88cddcdb
Artov <95948055+ArtemPPPP@users.noreply.github.com>
2022-04-15 13:53:00 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("Hello World!");
+const btn = document.getElementById('btn')
+const block = document.querySelector('.block')
+
+const scrollHeight = block.scrollHeight
+const scrollWidth = block.scrollWidth
+
+
+
+btn.addEventListener('click', () => {
+    const elemRect = block.getBoundingClientRect()
+
+    console.log(elemRect.top);
+    console.log(elemRect.Left);
+  
+})
