fd745451b4a607d801a9311895cda96889e66b52
Artov <95948055+ArtemPPPP@users.noreply.github.com>
2022-04-29 17:17:14 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-alert("Hello World!");
+let btn = document.querySelector('#btn')
+let text = document.querySelector('#text')
+let range = document.querySelector('#range')
+let circle = document.querySelector('#circle')
+let rangeSpan = document.querySelector('#range-span')
+let e_btn = document.querySelector('#e_btn')
+
+function start() {
+    rangeSpan.textContent = 50
+    circle.style.height = range.value + '%'
+    circle.style.width = range.value + '%'
+    e_btn.style.display = 'none'
+}
+
+
+const color = function() {
+    btn.style.backgroundColor = text.value
+}
+
+
+const position = function(){
+    circle.style.height = range.value + '%'
+    circle.style.width = range.value + '%'
+    rangeSpan.textContent = event.target.value
+}
+
+start()
+btn.addEventListener('click', color)
+range.addEventListener('input', position)
