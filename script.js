0e5877946c689da2bcc88b19e8242a919d2d2064
Artov <95948055+ArtemPPPP@users.noreply.github.com>
2022-04-29 17:19:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert("Hello World!");
+class First {
+    hello() {
+        console.log("Привет я метод родителя!");
+    }
+}
+
+class Seconds extends First {
+    hello() {
+        super.hello()
+        console.log("А я наследуемый метод!");
+    }
+}
+
+const second = new Seconds
+
+second.hello()
